3c0ffaeaa798c301310933314c6b37945d3eb419
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-11-03 23:26:26 +0100

    sorted a string using string method

---
+++ sortByChar.js
@@ -0,0 +1,15 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+// example = 'man i need a taxi up to ubud' => ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+
+
+function lastChar(last){
+    return last.split(' ').sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length -1))
+}
+
+
+console.log(lastChar("man i need a taxi up to ubud"))
