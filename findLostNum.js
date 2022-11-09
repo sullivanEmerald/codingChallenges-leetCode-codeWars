7caa48088c0f7c8baa227a7f5b53effc77a77964
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-11-09 17:09:27 +0100

    added more approach to solve the agorithmn and added arrow function

--- findLostNum.js
+++ findLostNum.js
@@ -20,4 +20,25 @@ function findDeletedNumber(arr, mixArr){
     }
 }
 
-console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]) , "2")
+// console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]) , "2")
+
+function findDeletedNum(arr, mixArr){
+    return arr.filter(item => mixArr.indexOf(item) === -1)
+}
+
+// console.log(findDeletedNum([1,2,3,4,5], [3,4,1,5]))
+
+
+const findnum = (arr, mixArr) => arr.length === mixArr.length ? 0 : arr.filter(item => mixArr.indexOf(item) === -1)[0]
+
+console.log(findnum( [1,2,3,4,5], [3,4,1,5]))
+
+
+function numsDelection(arr, mixArr){
+    let num1 =  arr.reduce((a,b) => a + b)
+    let num2 = mixArr.reduce((a,b) => a + b)
+
+    return num1 - num2
+}
+
+console.log(numsDelection([1,2,3,4,5], [3,4,1,5]))
