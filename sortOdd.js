d4d822ccd461c501e8bb2d11709ab70fe2ce99aa
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-11-16 01:01:07 +0100

    used shift and pop to solve the algorithm

--- sortOdd.js
+++ sortOdd.js
@@ -1,10 +1,26 @@
 // You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+    // linear complexity
+    // function sortOdd(arr){
+    //     let result = arr.filter(item => item % 2 !== 0).sort((a,b) => a - b)
+    //     console.log(result)
+
+    //     return arr.map(item => item % 2 == 0 ? item : result.shift())
+    // }
+
+    // constantaine
+    function sortOdd(arr){
+        let result = arr.filter(item => item % 2 !== 0).sort((a,b) => b - a)
+        console.log(result)
+
+        return arr.map(item => item % 2 == 0 ? item : result.pop())
+    }
 
 
-function sortOdd(arr){
-    return arr.filter(item => item % 2 !== 0)
-}
 
 
-console.log(sortOdd([5, 8, 6, 3, 4]))
+console.log(sortOdd([5, 8, 6, 3, 4, 17])) 
