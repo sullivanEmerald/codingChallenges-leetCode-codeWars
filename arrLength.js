d13bed1f15b8499ef2ecb0a1d4c8949d394e5f70
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-11-18 00:17:31 +0100

    optimal solution

--- arrLength.js
+++ arrLength.js
@@ -8,9 +8,17 @@ function strArr(arr){
 
     let result =  Math.round(arr.reduce((a,b) => a + b).length / arr.length)
 
-    return arr.map(item => item.length === result ? item : item += item.split('')[0])
-    
-    // return arr.map(item => item.length  < result )
+    // return arr.map(item => {
+    //     if(item.length === result) return item
+    //     if (item.length < result) return item += item.split('')[0]
+    //     if (item.length > result) return item.slice(0, 3)
+        
+    // }) 
+
+    return arr.map(str => str[0].repeat(result))
+    // // return arr.map(item => item.length  < result )
 }
 console.log(strArr(['aa', 'bbb', 'cccc']))
 console.log(strArr(['aa', 'bb', 'ddd', 'eee']))
+console.log(strArr(['u', 'y']))
+
