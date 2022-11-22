517b8eb84ddbf991910ef43a24362c735029e7d9
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-11-22 21:40:15 +0100

    brute forcing the problem

--- filterString.js
+++ filterString.js
@@ -5,8 +5,23 @@
 // --> "alpha beta gamma delta alpha beta gamma delta"
 
 
+// function duplicateString(string){
+//     return string.split(' ').filter((item, i , arr) => item !== arr[i + 1]).join('')
+// }
+
 function duplicateString(string){
-    return string.split(' ').filter((item, i , arr) => item !== arr[i + 1]).join(' ')
+    
+    let array = []
+    let word =  string.split(" ")
+
+    for(let i = 0; i < word.length; i++){
+        
+        if(word[i] !== word[i - 1]){
+            array.push(word[i])
+        }
+    }
+
+    return array.join( ' ')
 }
 
 console.log(duplicateString( 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
