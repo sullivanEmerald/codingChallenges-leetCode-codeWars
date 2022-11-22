7036cd9bfd3a762a22de04cd05cf23f7cd9e05b8
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-11-22 21:23:02 +0100

    removing consecutive duplicate words in a string

---
+++ filterString.js
@@ -0,0 +1,12 @@
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+
+function duplicateString(string){
+    return string.split(' ').filter((item, i , arr) => item !== arr[i + 1])
+}
+
+console.log(duplicateString( 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'))
