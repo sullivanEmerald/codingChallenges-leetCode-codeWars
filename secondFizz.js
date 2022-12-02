413a889698b2beeae3848d5dfdea790f32da04e8
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-12-02 20:56:29 +0100

    solving the fizz buss

---
+++ secondFizz.js
@@ -0,0 +1,17 @@
+// Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”.
+
+
+function fizzBuzz(num){
+for(let i = 0; i <= num; i++){
+    if(i % 2 === 0 && i % 3 === 0){
+        console.log('fizz buzz')
+    }else if(i % 2 === 0){
+        console.log('fizz')
+    }else if(i % 3 === 0){
+        console.log('buzz')
+    }else{
+        console.log(i)
+    }
+}   
+}   
+console.log(fizzBuzz(20))
